7aadd0ad47000aec1576019fe4d3214b6295857c
Avinash <avinashrawat1705@gmail.com>
2025-04-04 01:27:49 +0530

    first commit

---
+++ lib/domain/appConsatant.dart
@@ -0,0 +1,7 @@
+
+
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color scaffoldbackgroud=Color(0XFFF7CB45);
+}
---
+++ lib/ui/screens/splash/splashScreen.dart
@@ -0,0 +1,44 @@
+
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:swiggy/domain/appConsatant.dart';
+import 'package:swiggy/ui/widgets/uihelper.dart';
+
+import '../login/loginScreen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  
+  @override
+  void initState() {
+    Timer(Duration(seconds: 2),() {
+      Get.to(LoginScreen());
+    });
+    super.initState();
+  }
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.scaffoldbackgroud,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            UiHelper.CustomImage(img: "image 1 (1).png")
+          ],
+        ),
+      ),
+    );
+  }
+}
