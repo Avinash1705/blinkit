928b58f537c7aec7c09f721353e73f3fe55bb3a5
Bama Kant Kar <bamakantkar@gmail.com>
2025-08-26 10:48:56 +0530

    added key file

--- apk/android/app/build.gradle.kts
+++ apk/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.apk"
+    namespace = "com.luffy1705.fluxkart"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.apk"
+        applicationId = "com.luffy1705.fluxkart"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- apk/android/app/src/main/kotlin/com/luffy1705/fluxkart/MainActivity.kt
+++ apk/android/app/src/main/kotlin/com/luffy1705/fluxkart/MainActivity.kt
@@ -1,5 +1,7 @@
-package com.example.apk
+package com.luffy1705.fluxkart
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
+
+
